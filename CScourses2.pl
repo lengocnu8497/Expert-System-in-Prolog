833c9724d8bc2d077a5dd4ca21112c19c610b76a
isabelpaz1999 <44704905+isabelpaz1999@users.noreply.github.com>
2020-05-02 15:49:16 -0700

    Fixed knowledge base

--- CScourses2.pl
+++ CScourses2.pl
@@ -4,7 +4,6 @@ top_goal(X):-
 	eligible(X).
 
 % experiment
-%MODIFY 1300
 eligible(cs1300):-
 	not(known(yes,cscourse,cs1300)),
 	(known(yes,cscourse,mat1050),known(yes,cscourse,mat1060);
@@ -73,32 +72,20 @@ eligible(cs4800):-
 	
 eligible(mat2250):-
 	not(known(yes,cscourse,mat2250)),
-	(known(yes,year,sophomore);
-	known(yes,year,junior);
-	known(yes,year,senior),
-	known(yes,cscourse,mat1150)).
+	known(yes,cscourse,mat1150).
 	
 eligible(phy1510):-
 	not(known(yes,cscourse,phy1510)),
-	(known(yes,year,sophomore);
-	known(yes,year,junior);
-	known(yes,year,senior),
-	known(yes,cscourse,mat1140);
+	(known(yes,cscourse,mat1140);
 	known(yes,cscourse,mat1150)).
 	
 eligible(phy1520):-
 	not(known(yes,cscourse,phy1520)),
-	(known(yes,year,sophomore);
-	known(yes,year,junior);
-	known(yes,year,senior),
-	known(yes,cscourse,phy1510)).
+	(known(yes,cscourse,phy1510)).
 	
 eligible(sta2260):-
 	not(known(yes,cscourse,sta2260)),
-	(known(yes,year,sophomore);
-	known(yes,year,junior);
-	known(yes,year,senior),
-	known(yes,cscourse,mat1150);
+	(known(yes,cscourse,mat1150);
 	known(yes,cscourse,mat1310)).
 
 eligible(bio1110):-
@@ -177,13 +164,11 @@ eligible(cs4200):-
 
 eligible(cs4210):-
 	not(known(yes,cscourse,cs4210)),
-	(known(yes,cscourse,cs2410);
-	known(yes,cscourse,cs3010)).
+	(known(yes,cscourse,cs3010)).
 
 eligible(cs4250):-
 	not(known(yes,cscourse,cs4250)),
-	(known(yes,cscourse,cs2410);
-	known(yes,cscourse,cs2400)).
+	(known(yes,cscourse,cs2400)).
 
 eligible(cs4350):-
 	not(known(yes,cscourse,cs4350)),
@@ -192,7 +177,7 @@ eligible(cs4350):-
 eligible(cs4450):-
 	not(known(yes,cscourse,cs4450)),
 	(known(yes,cscourse,cs2400),
-	known(es,cscourse,cs2560)).
+	known(yes,cscourse,cs2560)).
 
 eligible(cs4500):-
 	not(known(yes,cscourse,cs4500)),
@@ -246,7 +231,7 @@ cs3110:-
 	
 cs3310:-
 	cs2400,
-	mat2250,
+	sta2260,
 	asserta(known(yes,cscourse,cs3310)).
 	
 cs3650:-
@@ -269,6 +254,7 @@ cs4630:-
 	asserta(known(yes,cscourse,cs4630)).
 	
 cs4800:-
+	cs2400,
 	asserta(known(yes,cscourse,cs4800)).
 	
 mat2250:-
@@ -287,6 +273,7 @@ phy1520:-
 	
 sta2260:-
 	mat1310,
+	mat1150,
 	asserta(known(yes,cscourse,sta2260)).
 
 mat1150:-
@@ -300,6 +287,113 @@ mat1310:-
 	mat1150,
 	asserta(known(yes,cscourse,mat1310)).
 
+mat1050:-
+	asserta(known(yes,cscourse,mat1050)).
+
+mat1060:-
+	mat1050
+	asserta(known(yes,cscourse,mat1060)).
+
+bio1110:-
+	bio1110l,
+	asserta(known(yes,cscourse,bio1110)).
+
+bio1110l:-
+	asserta(known(yes,cscourse,bio1110l)).
+
+cs1260:-
+	mat1150,
+	asserta(known(yes,cscourse,cs1260)).
+
+cs1280:-
+	mat1150,
+	asserta(known(yes,cscourse,cs1280)).
+
+cs2450:-
+	cs1400,
+	asserta(known(yes,cscourse,cs2450)).
+
+cs2560:-
+	cs1280,
+	cs1400,
+	asserta(known(yes,cscourse,cs2560)).
+
+cs2600:-
+	cs1280,
+	cs1400,
+	asserta(known(yes,cscourse,cs2600)).
+
+cs3010:-
+	mat1150,
+	cs2400,
+	asserta(known(yes,cscourse,cs3010)).
+
+cs3520:-
+	cs2400,
+	asserta(known(yes,cscourse,cs3520)).
+
+cs3560:-
+	cs2400,
+	asserta(known(yes,cscourse,cs3560)).
+
+cs3700:-
+	cs3310,
+	asserta(known(yes,cscourse,cs3700)).
+
+cs3800:-
+	cs2640,
+	asserta(known(yes,cscourse,cs3800)).
+
+cs4110:-
+	cs3110,
+	asserta(known(yes,cscourse,cs4110)).
+	
+cs4200:-
+	cs3310,
+	asserta(known(yes,cscourse,cs4200)).
+
+cs4210:-
+	cs3010,
+	asserta(known(yes,cscourse,cs3010)).
+	
+cs4250:-
+	cs2400,
+	asserta(known(yes,cscourse,cs4250)).
+
+cs4350:-
+	cs2400,
+	asserta(known(yes,cscourse,cs4350)).
+
+cs4450:-
+	cs2560,
+	cs2400,
+	asserta(known(yes,cscourse,cs4450)).
+
+cs4500:-
+	cs3110,
+	asserta(known(yes,cscourse,cs4500)).
+
+cs4600:-
+	cs2400,
+	asserta(known(yes,cscourse,cs4600)).
+
+cs4650:-
+	cs2400,
+	asserta(known(yes,cscourse,cs4650)).
+
+cs4700:-
+	cs2400,
+	asserta(known(yes,cscourse,cs4700)).
+
+cs4750:-
+	cs2400,
+	asserta(known(yes,cscourse,cs4750)).
+
+cs4810:-
+	cs4350,
+	cs4800,
+	asserta(known(yes,cscourse,cs4810)).
+	
 % class standing inference	
 
 senior:-
